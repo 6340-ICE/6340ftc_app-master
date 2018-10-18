9589cf8fc2861ecbe428d59f9c937453066dc5aa
deanlange <langed@wdmcs.org>
2018-10-17 20:32:05 -0500

    BlueCrater needs fixing on main turn

--- 6340TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCrater.java
+++ 6340TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCrater.java
@@ -26,7 +26,7 @@ BlueCrater extends Team6340Controls {
 
         // Insert Autonomous Code Here
         gyroTurn(TURN_SPEED, 45, 5);                     // Turn 45 degrees left
-        gyroDrive(DRIVE_SPEED, 42, 45, 10);     // Move Forward 42"
+        gyroDrive(DRIVE_SPEED, 47, 45, 10);     // Move Forward 42"
         gyroTurn(TURN_SPEED, 130, 5);                   // Turn Left 90 degrees (Heading 130)
         gyroDrive(DRIVE_SPEED, 66, 130, 100);   // Move forward 66"
         trophy.setPosition(.75);// Deliver Trophy
